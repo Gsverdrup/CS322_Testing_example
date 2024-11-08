9f062192e136aee7683f1d2ca042f5c165bf1d92
Gsverdrup <136853503+Gsverdrup@users.noreply.github.com>
2024-11-08 10:41:46 -0800

    Add files via upload

--- test_bank.py
+++ test_bank.py
@@ -16,3 +16,20 @@ def test_withdraw():
     account2 = Account("Owner2", 5)
     account2.withdraw(3.2)
     assert account2.balance == 1.8
+
+def test_deposit_negative_amount():
+    account1 = Account("Owner1")
+    with pytest.raises(ValueError):        
+        account1.deposit(-5.5)
+    
+    
+
+def test_withdraw_more_than_balance():
+    account1 = Account("Owner1", 1)
+    account1.withdraw(2)
+    assert pytest.raises(Exception)
+
+def test_withdraw_negative_amount():
+    account1 = Account("Owner1", 1)
+    account1.withdraw(-2)
+    assert pytest.raises(Exception)
